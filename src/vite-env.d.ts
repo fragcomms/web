ee312e3a06465e3f9ccdc38667f45e33f0ac05bd
default <jdarby@unr.edu>
2025-12-10 05:25:55 +0000

    Revert "config changes and shader"
    This reverts commit e44b855e922aeb6869dbe79f8b6eb294da39e110.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-/// <reference types="@webgpu/types" />
-
-declare module '*wgsl?raw' {
-    const source: string;
-    export default source;
-}
