2cf896fb9fc4a6ca949b5806bcb2522c50d57bce
default <hatomemes@gmail.com>
2026-02-11 14:34:57 +0000

    fully refactor backend to use typescript only. removed python implementation and separated all routes to their respective files. decentralized the majority of server.ts to make sure it is digestible code

---
+++ backend/routes/replay.ts
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import pool from '../config/db.js';
+import { ensureAuth } from '../middleware/authentication.js'
+import { DiscordProfile as User } from '../types/user.js';
+
+const router = Router();
+
+// /api/replay
+router.get("/", ensureAuth, async (req, res) => {
+  const user = req.user as User;
+  if (!user) return res.status(401).send("Unauthorized");
+  try {
+    const query = `
+      SELECT r.replay_id, r.name, d.fetch_time
+      FROM replays r
+      JOIN demos d ON d.demo_id = r.demo_id
+      JOIN media_access ma ON r.audio_id = ma.audio_id
+      WHERE ma.discord_id = $1
+      ORDER BY d.fetch_time DESC`;
+    
+    const result = await pool.query(query, [user.discord_id]);
+    res.json(result.rows);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Database error");
+  }
+});
+
+// /api/replay/id
+router.get("/:id", ensureAuth, async (req, res) => {
+  const user = req.user as User;
+  if (!user) return res.status(401).send("Unauthorized");
+  try {
+    const query = `
+      SELECT r.replay_id, r.name, d.fetch_time, d.file_path
+      FROM replays r
+      JOIN demos d ON d.demo_id = r.demo_id
+      JOIN media_access ma ON ma.audio_id = r.audio_id
+      WHERE r.replay_id = $1 AND ma.discord_id = $2`;
+
+    const result = await pool.query(query, [req.params.id, user.discord_id]);
+    if (result.rows.length === 0) return res.status(404).send("Replay not found");
+    res.json(result.rows[0]); // 1 for now
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Database error");
+  }
+});
+
+//TODO: allow user to create a replay
+
+export default router;
---
+++ backend/types/user.ts
@@ -0,0 +1,12 @@
+export interface DiscordProfile {
+  discord_id: string;
+  username: string;
+  discriminator: string;
+  avatar: string | null;
+  email?: string;
+  global_name?: string;
+}
+
+export interface UserWithToken extends DiscordProfile{
+  token: string;
+}
