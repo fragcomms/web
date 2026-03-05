7ca3ef8a3cde5d212cbb8764cac2fa9ce48995b6
default <shannon.belton97@gmail.com>
2026-03-05 06:33:40 +0000

    updated tests

--- src/pages/login-form.test.tsx
+++ src/pages/login-form.test.tsx
@@ -59,6 +59,7 @@ describe("LoginForm", () => {
     it("shows 'Continue with Discord' button when user not logged in", () => {
         render(<LoginForm />);
         
+        // expected discord login button
         const button = screen.getByRole("button", { name: /continue with Discord/i });
         expect(button).toBeInTheDocument();
         expect(button).not.toBeDisabled();
@@ -68,7 +69,8 @@ describe("LoginForm", () => {
     // #2 TEST LOGIN BUTTON DISABLED WHEN CLICKED
     it("shows disabled button that says 'Connecting...' state when testLoading is true", () => {
         render(<LoginForm testLoading={true} />);
-
+        
+        // expected button disabled
         const button = screen.getByRole("button");
         expect(button).toBeInTheDocument();
         expect(button).toBeDisabled();
@@ -88,11 +90,11 @@ describe("LoginForm", () => {
 
         render(<LoginForm />);
 
-        // User info
+        // user info
         expect(screen.getByText(/testuser/i)).toBeInTheDocument();
         expect(screen.getByText(/test@test.com/i)).toBeInTheDocument();
 
-        // Buttons
+        // buttons
         expect(screen.getByText(/view profile data/i)).toBeInTheDocument();
         expect(screen.getByText(/view connections/i)).toBeInTheDocument();
         expect(screen.getByText(/logout/i)).toBeInTheDocument();
