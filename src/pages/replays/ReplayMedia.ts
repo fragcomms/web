71adb70e3ea2f7b49e7ecb85a298a5577249a752
default <hatomemes@gmail.com>
2026-04-05 18:11:25 -0700

    add end times to transcript segment as well as fallback to old format of transcriptions

--- src/pages/replays/ReplayMedia.ts
+++ src/pages/replays/ReplayMedia.ts
@@ -6,6 +6,7 @@ export interface TranscriptSegment {
   start: number;
   end: number;
   text: string;
+  clean_text: string;
 }
 
 type ReplayMediaMetadata = {
@@ -65,7 +66,7 @@ export function useReplayMedia(id: string | undefined, audioPlayerRef: React.Ref
         // this makes it easier to display in the transcript panel and also ensures the audio tracks are properly loaded for all users mentioned in the transcripts
         for (const [discordId, segments] of Object.entries(transcriptJson)) {
           for (const seg of (segments as any[])) {
-            combined.push({ discordId, start: seg.start, end: seg.end, text: seg.text });
+            combined.push({ discordId, start: seg.start, end: seg.end, text: seg.raw_text || seg.text, clean_text: seg.clean_text });
           }
         }
 
