1c27811ae8ad01b4dbe5889162956855ababb8bd
default <shannon.belton97@gmail.com>
2026-04-09 02:37:30 -0700

    moved HP bar from replaypage to its own file (modularity)

---
+++ src/pages/replays/components/HPBar.tsx
@@ -0,0 +1,33 @@
+// hp bar interface
+interface HPBar {
+    hp: number;
+    alive: boolean;
+    ringColor: string;
+    centerTextColorClass: string;
+}
+
+// function for updating hp bar
+export default function HPBarUpdate({
+    hp,
+    alive,
+    ringColor,
+    centerTextColorClass,
+}: HPBar) {
+    const hpPercent = Math.max(0, Math.min(100, hp));
+    return (
+        <div
+            className="relative h-9 w-9 shrink-0 rounded-full"
+            style={{
+                background: `conic-gradient(${ringColor} ${hpPercent}%, rgb(148 163 184) ${hpPercent}% 100%)`,
+          
+            }}
+
+        >
+            <div
+                className={`absolute inset-0.75 flex items-center justify-center rounded-full bg-white text-[10px] font-semibold dark:bg-slate-900 ${centerTextColorClass}`}
+            >
+                {alive ? hp : "D"}
+            </div>
+        </div>
+    )
+}
